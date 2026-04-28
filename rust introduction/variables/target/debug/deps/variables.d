42eb6544cc1629ddd4b83aaf145021aba6eeb397
Mikhail-Tal-khal <khaledkhlyan@gmail.com>
2026-04-28 22:35:13 +0200

    Guesing game Rust

---
+++ rust introduction/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\LEMAR\Desktop\rust-projects\rust introduction\variables\target\debug\deps\variables.d: src\main.rs
+
+C:\Users\LEMAR\Desktop\rust-projects\rust introduction\variables\target\debug\deps\variables.exe: src\main.rs
+
+src\main.rs:
---
+++ rust introduction/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\LEMAR\Desktop\rust-projects\rust\ introduction\variables\target\debug\variables.exe: C:\Users\LEMAR\Desktop\rust-projects\rust\ introduction\variables\src\main.rs
